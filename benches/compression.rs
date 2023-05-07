2409d4735a9641c1f73f00a89d79828b2ed1b3d4
hov1417 <hovhannes1417@gmail.com>
2023-05-07 16:24:35 +0400

    feat: async compression

---
+++ benches/compression.rs
@@ -0,0 +1,161 @@
+#![cfg(test)]
+
+use std::env::temp_dir;
+use std::fs::File;
+use std::io::Write;
+use std::path::{Path, PathBuf};
+use std::sync::Arc;
+
+use criterion::{criterion_group, criterion_main, Criterion};
+use eyre::Context;
+use lazy_static::lazy_static;
+use rand::distributions::Alphanumeric;
+use rand::{thread_rng, Rng};
+use zstd::Encoder;
+
+use lupin::compression::compress_files;
+
+lazy_static! {
+    static ref FILES: Arc<[(String, Vec<u8>)]> =
+        get_files(100, 1_000_000).into();
+}
+
+pub fn finish_archive(writer: &mut impl Write) -> eyre::Result<()> {
+    writer.write_all(&[0; 1024]).context("could not write file")
+}
+
+pub fn write_header(
+    writer: &mut impl Write,
+    path: &String,
+    data: &Vec<u8>,
+) -> eyre::Result<()> {
+    let mut header = tar::Header::new_gnu();
+    header
+        .set_path(&path)
+        .context("could not set path for file")?;
+    header.set_mode(0o644);
+    header.set_size(data.len() as u64);
+    header.set_cksum();
+
+    writer
+        .write_all(header.as_bytes())
+        .context("could not write header")?;
+
+    // Pad with zeros if necessary.
+    let buf = [0; 512];
+    let remaining = 512 - (data.len() % 512);
+    if remaining < 512 {
+        writer
+            .write_all(&buf[..remaining as usize])
+            .context("could not write file")?;
+    }
+
+    Ok(())
+}
+
+pub fn blocking_compress_files(
+    files: &[(String, Vec<u8>)],
+    out_file: &Path,
+) -> eyre::Result<()> {
+    let file = File::create(out_file)
+        .with_context(|| format!("could not create file {:?}", out_file))?;
+
+    let mut writer =
+        Encoder::new(file, 10).context("could not create zst encoder")?;
+    writer.multithread(100)?;
+    for (path, data) in files {
+        write_header(&mut writer, path, data)
+            .context("could not write header")?;
+
+        writer.write_all(data).context("could not write file")?;
+    }
+    finish_archive(&mut writer)?;
+    writer
+        .finish()
+        .context("could not compress archive")?
+        .flush()
+        .context("could not flush buffer")?;
+    Ok(())
+}
+
+fn random_text(len: usize) -> String {
+    thread_rng()
+        .sample_iter(&Alphanumeric)
+        .take(len)
+        .map(char::from)
+        .collect()
+}
+
+fn get_files(file: usize, size: usize) -> Vec<(String, Vec<u8>)> {
+    let mut files = Vec::new();
+    for _ in 0..file {
+        files.push((random_text(10), random_text(size).into_bytes()));
+    }
+    files
+}
+
+fn benchmark_blocking_compress_files(c: &mut Criterion) {
+    let out_file = temp_dir().join("benchmark_blocking_compress_files.tar.zst");
+    c.bench_function("blocking_compress_files 100 files by one mb", |b| {
+        b.iter(|| blocking_compress_files(FILES.as_ref(), &out_file).unwrap())
+    });
+}
+
+fn benchmark_compress_files(c: &mut Criterion) {
+    let out_file = temp_dir().join("benchmark_compress_files.tar.zst");
+    c.bench_function("compress_files 100 files by one mb", |b| {
+        b.to_async(tokio::runtime::Runtime::new().unwrap())
+            .iter(|| compress_files(FILES.as_ref(), &out_file))
+    });
+}
+
+async fn blocking_compress_files_in_async(
+    files: Arc<[(String, Vec<u8>)]>,
+    out_file: Arc<PathBuf>,
+) {
+    tokio::task::spawn_blocking(move || {
+        blocking_compress_files(files.as_ref(), out_file.as_ref()).unwrap();
+    })
+    .await
+    .unwrap()
+}
+
+fn benchmark_blocking_compress_files_in_async(c: &mut Criterion) {
+    let out_file = Arc::new(
+        temp_dir().join("benchmark_blocking_compress_files_in_async.tar.zst"),
+    );
+    c.bench_function(
+        "blocking_compress_files_in_async 100 files by one mb",
+        |b| {
+            b.to_async(tokio::runtime::Runtime::new().unwrap())
+                .iter(|| {
+                    blocking_compress_files_in_async(
+                        Arc::clone(&FILES),
+                        Arc::clone(&out_file),
+                    )
+                })
+        },
+    );
+}
+
+criterion_group!(
+    name = bench_blocking_compress_files;
+    config = Criterion::default().sample_size(10);
+    targets = benchmark_blocking_compress_files
+);
+criterion_group!(
+    name = bench_compress_files;
+    config = Criterion::default().sample_size(10);
+    targets = benchmark_compress_files
+);
+criterion_group!(
+    name = bench_blocking_compress_files_in_async;
+    config = Criterion::default().sample_size(10);
+    targets = benchmark_blocking_compress_files_in_async
+);
+
+criterion_main!(
+    bench_blocking_compress_files,
+    bench_blocking_compress_files_in_async,
+    bench_compress_files
+);
--- src/compression.rs
+++ src/compression.rs
@@ -1,13 +1,16 @@
 use std::fs::File;
-use std::io::{BufWriter, Write};
-use std::path::PathBuf;
+use std::io::Write;
+use std::path::{Path, PathBuf};
+use std::time::Duration;
+use std::time::{SystemTime, UNIX_EPOCH};
 
+use async_compression::tokio::write::ZstdEncoder;
 use eyre::{bail, Context};
 use tar::Builder;
+use tokio::io::AsyncWriteExt;
 use tokio::task::spawn_blocking;
+use tracing::{error, warn};
 use zstd::Encoder;
-
-const BUF_SIZE: usize = 5 * 1024 * 1024;
 
 pub async fn compress_directory(
     src_dir: PathBuf,
@@ -20,23 +23,143 @@ pub async fn compress_directory(
         let file = File::create(&out_file)
             .with_context(|| format!("could not create file {:?}", out_file))?;
         let mut writer =
-            Encoder::new(BufWriter::with_capacity(BUF_SIZE, file), 10)
-                .context("could not create zst encoder")?;
+            Encoder::new(file, 10).context("could not create zst encoder")?;
         writer.multithread(100)?;
-        {
-            let mut tar_builder = Builder::new(writer);
-            tar_builder
-                .append_dir_all(".", src_dir)
-                .context("could not create archive")?;
-            let writer =
-                tar_builder.into_inner().context("error writing archive")?;
-            writer
-                .finish()
-                .context("could not compress archive")?
-                .flush()
-                .context("could not flush buffer")?;
-        }
+        let mut tar_builder = Builder::new(writer);
+        tar_builder
+            .append_dir_all(".", src_dir)
+            .context("could not create archive")?;
+        let writer =
+            tar_builder.into_inner().context("error writing archive")?;
+        writer
+            .finish()
+            .context("could not compress archive")?
+            .flush()
+            .context("could not flush buffer")?;
         Ok(())
     })
     .await?
+}
+
+pub struct Compressor {
+    writer: ZstdEncoder<tokio::fs::File>,
+    closed: bool,
+}
+
+impl Compressor {
+    pub async fn new(out_file: &Path) -> eyre::Result<Self> {
+        let file = tokio::fs::File::create(out_file)
+            .await
+            .with_context(|| format!("could not create file {:?}", out_file))?;
+        let writer = ZstdEncoder::with_quality_and_params(
+            file,
+            async_compression::Level::Precise(10),
+            &[async_compression::zstd::CParameter::nb_workers(100)],
+        );
+        Ok(Self {
+            writer,
+            closed: false,
+        })
+    }
+
+    pub async fn add_file(
+        &mut self,
+        name: &str,
+        data: &[u8],
+    ) -> eyre::Result<()> {
+        self.write_tar_header(name, data.len() as u64)
+            .await
+            .context("could not write header")?;
+
+        self.writer
+            .write_all(data)
+            .await
+            .context("could not write file")?;
+
+        Ok(())
+    }
+
+    pub async fn finish(&mut self) -> eyre::Result<()> {
+        self.finish_tar_archive().await?;
+        self.writer
+            .flush()
+            .await
+            .context("could not flush buffer")?;
+        self.writer
+            .shutdown()
+            .await
+            .context("could not compress archive")?;
+
+        self.closed = true;
+        Ok(())
+    }
+
+    async fn write_tar_header(
+        &mut self,
+        path: &str,
+        data_len: u64,
+    ) -> eyre::Result<()> {
+        let mut header = tar::Header::new_gnu();
+        header
+            .set_path(&path)
+            .context("could not set path for file")?;
+        header.set_mode(0o644);
+        header.set_mtime(get_unix_epoch());
+        header.set_size(data_len);
+        header.set_cksum();
+
+        self.writer
+            .write_all(header.as_bytes())
+            .await
+            .context("could not write header")?;
+
+        // Pad with zeros if necessary.
+        let buf = [0; 512];
+        let remaining = 512 - (data_len % 512);
+        if remaining < 512 {
+            self.writer
+                .write_all(&buf[..remaining as usize])
+                .await
+                .context("could not write file")?;
+        }
+
+        Ok(())
+    }
+
+    async fn finish_tar_archive(&mut self) -> eyre::Result<()> {
+        self.writer
+            .write_all(&[0; 1024])
+            .await
+            .context("could not write file")
+    }
+}
+
+impl Drop for Compressor {
+    fn drop(&mut self) {
+        if !self.closed {
+            error!("Compressor was not closed");
+        }
+    }
+}
+
+pub async fn compress_files(
+    files: &[(String, Vec<u8>)],
+    out_file: &Path,
+) -> eyre::Result<()> {
+    let mut compressor = Compressor::new(out_file).await?;
+    for (path, data) in files {
+        compressor.add_file(path, data).await?;
+    }
+    compressor.finish().await?;
+    Ok(())
+}
+
+fn get_unix_epoch() -> u64 {
+    SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .unwrap_or_else(|_| {
+            warn!("Time went backwards");
+            Duration::new(0, 0)
+        })
+        .as_secs()
 }
